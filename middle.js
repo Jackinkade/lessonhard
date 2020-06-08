817d4eec92f5629a96e28e0eb82554f8f6b663dc
master <shyngys99106@gmail.com>
2020-06-08 12:03:19 +0600

    2 урок усложненое задание

--- middle.js
+++ middle.js
@@ -1,2 +1,12 @@
 let num = 266219;
-console.log(num.split(","))
+let string = num + '';
+let arr = string.split('');
+let sum = arr.reduce(function(acc, item){
+    return acc * item
+}, 1)
+sum = sum **3;
+
+String(sum)
+let str = sum + '';
+
+console.log(str.substr(0, 2));
